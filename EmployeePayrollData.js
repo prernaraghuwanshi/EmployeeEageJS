190899c274e7feea6b298b28015df7a7f12f5425
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-11-13 11:07:01 +0530

    [Prerna]ADDED: EmployeePayrollData class with id, name and salary

---
+++ EmployeePayrollData.js
@@ -0,0 +1,26 @@
+class EmployeePayrollData {
+    // property
+    id;
+    salary;
+
+    constructor(id, name, salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        this._name = name;
+    }
+    toString() {
+        return "ID: " + this.id + " Name: " + this.name + " Salary: " + this.salary;
+    }
+}
+
+let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
+console.log(employeePayrollData.toString());
+employeePayrollData.name = "John";
+console.log(employeePayrollData.toString());
